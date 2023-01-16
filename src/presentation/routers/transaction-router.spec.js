85019834b5ce7b05be164e7a8a119863cd5f5e5a
wpaq <wallysonpabloo@gmail.com>
2023-01-16 15:13:11 -0300

    test: move sut creation to a factory helper method

--- src/presentation/routers/transaction-router.spec.js
+++ src/presentation/routers/transaction-router.spec.js
@@ -1,9 +1,13 @@
 const TransactionRouter = require('./transaction-router')
 const MissingParamError = require('../errors/missing-param-error')
 
+const makeSut = () => {
+  return new TransactionRouter()
+}
+
 describe('Transaction Router', () => {
   test('Should return 400 if no transaction amount is provided', async () => {
-    const sut = new TransactionRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         transactionDescription: 'any_description'
@@ -15,7 +19,7 @@ describe('Transaction Router', () => {
   })
 
   test('Should return 400 if no card number is provided', async () => {
-    const sut = new TransactionRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         transactionAmount: 'any_amount',
@@ -28,13 +32,13 @@ describe('Transaction Router', () => {
   })
 
   test('Should return 500 if no httpRequest is provided', async () => {
-    const sut = new TransactionRouter()
+    const sut = makeSut()
     const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpRequest has no body', async () => {
-    const sut = new TransactionRouter()
+    const sut = makeSut()
     const httpResponse = await sut.route({})
     expect(httpResponse.statusCode).toBe(500)
   })
